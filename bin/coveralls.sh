0cf75d6aabc4733c0762d3d7bb24ea04f42f1ea2
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-12-22 01:01:26 +0100

    Fix for coveralls

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -3,7 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if php -r "var_export(get_loaded_extensions(true));" | grep --quiet -i xdebug; then
-  php ${DIR}/../vendor/bin/coveralls -v
+  if [ -f ${DIR}/../dev-tools/vendor/bin/coveralls ]; then
+    php ${PHP_ARGS} ${DIR}/../dev-tools/vendor/bin/coveralls -v
+  else
+    php ${PHP_ARGS} ${DIR}/../dev-tools/vendor/bin/php-coveralls -v
+  fi
 else
   echo "Xdebug is not installed"
 fi
