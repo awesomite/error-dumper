b7871672c7af81be240f85f6f5d8c2ae3e7ead52
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-09-15 12:46:39 +0400

    Check if proper templates dir is used

---
+++ tests/DevTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the awesomite/error-dumper package.
+ *
+ * (c) Bartłomiej Krukowski <bartlomiej@krukowski.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Awesomite\ErrorDumper;
+
+/**
+ * @internal
+ */
+class DevTest extends TestBase
+{
+    public function testUseTemplatesDist()
+    {
+        $reflectionClass = new \ReflectionClass('Awesomite\ErrorDumper\Views\ViewHtml');
+        $file = \file_get_contents($reflectionClass->getFileName());
+        $this->assertContains("'templates_dist'", $file);
+    }
+}
