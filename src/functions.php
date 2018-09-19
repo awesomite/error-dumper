785a3b7e26643b3ab48334fa8e8594797ac5d4ef
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-09-19 22:57:20 +0400

    Added function Awesomite\ErrorDumper\exception_dump

---
+++ src/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the awesomite/error-dumper package.
+ *
+ * (c) Bartłomiej Krukowski <bartlomiej@krukowski.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Awesomite\ErrorDumper;
+
+use Awesomite\ErrorDumper\Serializable\SerializableException;
+use Awesomite\ErrorDumper\Views\ViewCli;
+use Awesomite\ErrorDumper\Views\ViewHtml;
+
+if (!\function_exists('Awesomite\ErrorDumper\exception_dump')) {
+    /**
+     * @param \Throwable|\Exception $exception
+     */
+    function exception_dump($exception)
+    {
+        $view = 'cli' === \php_sapi_name()
+            ? new ViewCli()
+            : new ViewHtml();
+
+        $view->display(new SerializableException($exception));
+    }
+}
