2f0f5dc1a4cb7701ed89954129ae84f89331b838
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-09-28 01:37:51 +0400

    Test if files in templates_dist are properly compressed

---
+++ tests/TemplatesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the awesomite/error-dumper package.
+ *
+ * (c) Bartłomiej Krukowski <bartlomiej@krukowski.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Awesomite\ErrorDumper;
+
+use Symfony\Component\Finder\Finder;
+
+/**
+ * @internal
+ */
+final class TemplatesTest extends AbstractTestCase
+{
+    public function testSingleLine()
+    {
+        $finder = new Finder();
+        $finder
+            ->in(\implode(\DIRECTORY_SEPARATOR, array(__DIR__, '..', 'templates_dist')))
+            ->name('*.twig');
+
+        $this->assertGreaterThan(0, \count($finder));
+        foreach ($finder as $file) {
+            $this->assertSame(1, \mb_substr_count($file->getContents(), "\n"));
+        }
+    }
+}
