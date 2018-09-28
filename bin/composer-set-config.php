d7c249a6a8bf7e85cabb302bab0b2fa36e0defc4
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-09-28 13:08:56 +0400

    Executed php-cs-fixer on bin files, execute bin/test.php in travis

--- bin/composer-set-config.php
+++ bin/composer-set-config.php
@@ -1,6 +1,15 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * This file is part of the awesomite/error-dumper package.
+ *
+ * (c) Bartłomiej Krukowski <bartlomiej@krukowski.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 global $argv;
 
 $handle = function ($scriptName, $key, $value) {
--- bin/test-optimized.php
+++ bin/test-optimized.php
@@ -1,6 +1,15 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * This file is part of the awesomite/error-dumper package.
+ *
+ * (c) Bartłomiej Krukowski <bartlomiej@krukowski.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Awesomite\ErrorDumper\Optimizer\Application as OptimizerApp;
 use Symfony\Component\Finder\Finder;
 
