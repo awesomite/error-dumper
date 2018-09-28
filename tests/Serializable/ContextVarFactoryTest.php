0a0607d9d22b873ba55f434dc9042075118baac6
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-09-28 15:56:35 +0400

    Added tests for ContextVarsFactory

--- tests/Serializable/ContextVarFactoryTest.php
+++ tests/Serializable/ContextVarFactoryTest.php
@@ -23,10 +23,58 @@ final class ContextVarFactoryTest extends AbstractTestCase
     {
         $factory = new ContextVarsFactory(new LightVarDumper());
         $vars = $factory->createContext();
+        $this->assertContextVars($vars);
+    }
+
+    public function testCreateForCli()
+    {
+        $factory = new ContextVarsFactory(new LightVarDumper());
+        $vars = $this->callPrivateMethod($factory, 'createForCli');
+        $this->assertContextVars($vars);
+    }
+
+    public function createForHttp()
+    {
+        $factory = new ContextVarsFactory(new LightVarDumper());
+        $vars = $this->callPrivateMethod($factory, 'createForHttp');
+        $this->assertContextVars($vars);
+    }
+
+    public function testExclude()
+    {
+        $factoryWithArgv = new ContextVarsFactory(new LightVarDumper());
+        $namesWithArgv = \array_map(
+            function (ContextVarInterface $var) {
+                return \ltrim($var->getName(), '$');
+            },
+            $this->callPrivateMethod($factoryWithArgv, 'createForCli')
+        );
+        $this->assertContains('argv', $namesWithArgv);
+
+        $factoryWithoutArgv = new ContextVarsFactory(new LightVarDumper(), array('args'));
+        $namesWithoutArgv = \array_map(
+            function (ContextVarInterface $var) {
+                return \ltrim($var->getName(), '$');
+            },
+            $this->callPrivateMethod($factoryWithoutArgv, 'createForCli')
+        );
+        $this->assertNotContains('args', $namesWithoutArgv);
+    }
+
+    private function assertContextVars($vars)
+    {
         $this->assertInternalType('array', $vars);
         $this->assertGreaterThan(0, \count($vars));
         foreach ($vars as $var) {
             $this->assertInstanceOf('Awesomite\ErrorDumper\Serializable\ContextVarInterface', $var);
         }
+    }
+
+    private function callPrivateMethod($object, $method, $args = array())
+    {
+        $reflectionMethod = new \ReflectionMethod($object, $method);
+        $reflectionMethod->setAccessible(true);
+
+        return $reflectionMethod->invokeArgs($object, $args);
     }
 }
