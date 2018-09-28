40d5b487172fc8d44ba839ba89e003482b014a6b
bkrukowski <krukowski.bartlomiej@gmail.com>
2018-09-28 23:26:27 +0400

    Check current exception handler after each test

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -18,17 +18,29 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
 {
     private $errorHandler;
 
+    private $exceptionHandler;
+
     protected function setUp()
     {
         parent::setUp();
         $this->expectOutputString('');
         $this->errorHandler = $this->getCurrentErrorHandler();
+        $this->exceptionHandler = $this->getCurrentExceptionHandler();
     }
 
     protected function tearDown()
     {
         parent::tearDown();
-        $this->assertSame($this->errorHandler, $this->getCurrentErrorHandler(), 'Error handler has changed');
+        $this->assertSame(
+            $this->errorHandler,
+            $this->getCurrentErrorHandler(),
+            'Error handler has changed'
+        );
+        $this->assertSame(
+            $this->exceptionHandler,
+            $this->getCurrentExceptionHandler(),
+            'Exception handler has changed'
+        );
     }
 
     private function getCurrentErrorHandler()
@@ -36,6 +48,15 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
         $current = \set_error_handler(function () {
         });
         \restore_error_handler();
+
+        return $current;
+    }
+
+    private function getCurrentExceptionHandler()
+    {
+        $current = \set_exception_handler(function () {
+        });
+        \restore_exception_handler();
 
         return $current;
     }
